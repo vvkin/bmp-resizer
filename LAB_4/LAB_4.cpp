3be6a0252902bdee225b2e91e16a4faee313b2ef
DenisioMytnysiano <den.mytnyk@ua.fm>
2020-05-12 22:35:11 +0300

    Main implemented

--- LAB_4/LAB_4.cpp
+++ LAB_4/LAB_4.cpp
@@ -1,7 +1,15 @@
 #include "BMPReader.h"
+#include <iostream>
 
-int main(){
-	BMPReader bmp_file("img\\balls.bmp");
-	bmp_file.resize("out.bmp", 10);
+int main(int argc, char* argv[])
+{
+    if (argc != 4) {
+        std::cout << "Incorrect number of args\n";
+        return 0;
+    }
+    BMPReader bitmapFile(argv[1]);
+    std::cout << "Resizing image " << atof(argv[3]) << " times\n";
+    bitmapFile.resize(argv[2], atof(argv[3]));
+    std::cout << "Done\nResult written to " << argv[2] << '\n';
 }
 
